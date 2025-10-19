7783e9e3107daa1c9e852196b2f4b49c15dad83c
Luis Teixeira Lima <dev.louwis@gmail.com>
2025-10-19 03:42:00 +0200

    adding .gitignore

--- dist/schemas/common.js
+++
@@ -1,5 +0,0 @@
-// src/schemas/common.ts
-import { z } from "zod";
-export const IncludeQuerySchema = z.object({
-    include: z.string().optional(),
-});
--- dist/schemas/index.js
+++
@@ -1,4 +0,0 @@
-export * as CountrySchema from "./country";
-export * as LeagueSchema from "./league";
-export * as SportSchema from "./sport";
-export * from "./common";
